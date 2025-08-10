8a5e80facce8bfa9c6f696abe2c46033ae5ede0d
Aleksandr Lipunov <alipunov@mail.ru>
2025-08-10 17:33:39 +0000

    task 2.2 m2m-relations completed

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope
 
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_main = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                count_main += 1
+
+        if count_main > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        if count_main == 0:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,26 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='РАЗДЕЛ')
+    is_main = models.BooleanField(default=False, verbose_name='ОСНОВНОЙ')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статей'
+        unique_together = ('article', 'tag')  # Уникальность пары статья-тег
+
+    def __str__(self):
+        return f"{self.article.title} - {self.tag.name}"
