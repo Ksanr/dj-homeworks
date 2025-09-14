1703b591da214cf8e636583be0f799e2ecbbd3c4
Aleksandr Lipunov <alipunov@mail.ru>
2025-09-14 01:43:12 +0000

    added permissions for admin

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -5,6 +5,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from .filters import AdvertisementFilter
 from .models import Advertisement
+from .permissions import IsOwnerOrAdminOrReadOnly
 from .serializers import AdvertisementSerializer
 
 
@@ -23,6 +24,8 @@ class AdvertisementViewSet(ModelViewSet):
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create"]:
             return [IsAuthenticated()]
+        if self.action in ["update", "partial_update", "destroy"]:
+            return [IsOwnerOrAdminOrReadOnly()]
         return []
